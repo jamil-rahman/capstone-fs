2d00b4eb6d847cb6acdf32e7e706219584a8a8c2
Prince-Automation <87142711+Prince-Automation@users.noreply.github.com>
2024-10-11 17:46:11 -0400

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+// import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Body from './components/Body';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import CreatePost from './components/CreatePost';
+import Profile from './components/Profile';
+import Insight from './components/Insight';
+import AuthLayout from './components/AuthLayout';
+
+const router = createBrowserRouter([
+  
+  {
+    path: "/auth",
+    element: <AuthLayout />,  // Layout for authentication pages
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+    ],
+  },
+  {
+    path: '/',
+    element: <Body />,
+    children: [
+      {
+        path: "/createpost",
+        element: <CreatePost />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/insight",
+        element: <Insight />,
+      },
+      {
+          path: "*",
+          element: <p>404 Page Not Found</p>,
+      },
+    ],
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
